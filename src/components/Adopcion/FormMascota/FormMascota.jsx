0165a16a2a009c1954dbe16baf257a6cdc490560
Lucas Ledesma <lucasledesma27@gmail.com>
2022-04-28 08:11:36 -0300

    formv2

--- src/components/Adopcion/FormMascota/FormMascota.jsx
+++ src/components/Adopcion/FormMascota/FormMascota.jsx
@@ -1,33 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useFireStore from '../../../hooks/useFireStore';
 
 const FormMascota = () => {
+
+
+    const [form, setForm] = useState({
+
+
+        buyer: {
+            nombre: "",
+            edad: "",
+            color: "",
+            idCategory: "",
+            tamaño: "",
+            urlFoto: "",
+            enfermedad: "",
+            vacunas: "",
+            descripcion: "",
+        },
+        
+    })
+    const { generateAdopc, idAdopc } = useFireStore();
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            buyer: {
+                ...form.buyer,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        generateAdopc({ datos: form })
+        
+    }
+
     return (
-        <form class="row g-3 needs-validation" novalidate>
+        <form class="row g-3 needs-validation" onSubmit={handleSubmit} novalidate>
             <h3>Formulario para publicar mascota</h3>
             <div class="col-md-3">
                 <label for="validationCustom01" class="form-label">Nombre</label>
-                <input type="text" class="form-control" id="validationCustom01" required></input>
+                <input onChange={handleChange} name="nombre" value={form.buyer.nombre} type="text" class="form-control" id="validationCustom01" required></input>
                 <div class="valid-feedback">
                     Atencion!
                 </div>
             </div>
             <div class="col-md-3">
                 <label for="validationCustom02" class="form-label">Edad</label>
-                <input type="number" class="form-control" id="validationCustom02" required></input>
+                <input onChange={handleChange}name="edad" value={form.buyer.edad} type="number" class="form-control" id="validationCustom02" required></input>
                 <div class="valid-feedback">
                     Atencion!
                 </div>
             </div>
             <div class="col-md-3">
-                <label for="validationCustom03" class="form-label">Localizacion</label>
-                <input type="text" class="form-control" id="validationCustom03" required></input>
+                <label for="validationCustom03" class="form-label">color</label>
+                <input onChange={handleChange}name="color" value={form.buyer.color} type="text" class="form-control" id="validationCustom03" required></input>
                 <div class="invalid-feedback">
                     Atencion!
                 </div>
             </div>
             <div class="col-md-3">
                 <label for="validationCustom04" class="form-label">Mascota</label>
-                <select class="form-select" id="validationCustom04" required>
+                <select onChange={handleChange}name="idCategory" value={form.buyer.idCategory} class="form-select" id="validationCustom04" required>
                     <option selected disabled value="">Tu mascota es..</option>
                     <option>Gata/o</option>
                     <option>Perra/o</option>
@@ -39,7 +76,7 @@ const FormMascota = () => {
             </div>
             <div class="col-md-3">
                 <label for="validationCustom04" class="form-label">Tamaño</label>
-                <select class="form-select" id="validationCustom04" required>
+                <select onChange={handleChange}name="tamaño" value={form.buyer.tamaño} class="form-select" id="validationCustom04" required>
                     <option selected disabled value="">Tu mascota es..</option>
                     <option>Pequeña/o</option>
                     <option>Mediana/o</option>
@@ -53,21 +90,21 @@ const FormMascota = () => {
                 <label for="basic-url" class="form-label">Url de foto</label>
             <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon3">https://instagram.com/user/photo3</span>
-                <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3"></input>
+                <input onChange={handleChange}name="urlFoto" value={form.buyer.urlFoto} type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3"></input>
             </div>
             </div>
             
             <div class="form-check form-switch">
-                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault"></input>
+                <input onChange={handleChange}name="vacunas" value={form.buyer.vacunas} class="form-check-input" type="checkbox" id="flexSwitchCheckDefault"></input>
                 <label class="form-check-label" for="flexSwitchCheckDefault">Esta Vacunada/o.</label>
             </div>
             <div class="form-check form-switch">
-                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault"></input>
+                <input onChange={handleChange}name="enfermedad" value={form.buyer.enfermedad} class="form-check-input" type="checkbox" id="flexSwitchCheckDefault"></input>
                 <label class="form-check-label" for="flexSwitchCheckDefault">Esta Enferma/o.</label>
             </div>
             <div class="col-md-12">
                     <label for="validationCustom05" class="form-label">Descripcion</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <textarea onChange={handleChange}name="descripcion" value={form.buyer.descripcion} class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
             <div class="col-12">
                 <div class="form-check">
@@ -81,7 +118,7 @@ const FormMascota = () => {
                 </div>
             </div>
             <div class="col-12">
-                <button class="btn btn-primary" type="submit">Publicar</button>
+                <button disabled={!form.buyer.nombre || !form.buyer.color || !form.buyer.urlFoto}  onClick={ generateAdopc()} class="btn btn-primary" type="submit">Publicar</button>
             </div>
         </form>
     )
